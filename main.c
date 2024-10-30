4e43df9d0e2dc0e07e6adb51da194a494c311d59
frank <gihurdev@gmail.com>
2024-10-30 09:46:23 +0100

    basic insert

--- main.c
+++ main.c
@@ -10,31 +10,55 @@ typedef struct Node Node;
 
 //array of nodes with length. Points to the first node.
 typedef struct {
-    Node *start_node;
+    char data;
+    int freq;
+} FreqElem;
+
+typedef struct {
+    FreqElem *first_elem;
     int length;
-} NodeArray;
+} FreqArray;
 
 //insert a char into left or right of node:
 //depending on the value of pos
-Node *insert_char(Node node,char c,int pos);
+void insert_char(Node *node,char c,int pos) {
+    switch (pos) {
+        case 0: node->left_data=c; break;
+        case 1: node->right_data=c; break;
+    }
+}
 
 //add a node below another
-//returns the pointer to the parent node
-Node *insert_node(Node parent, Node child, int pos);
+void insert_node(Node *parent, Node *child, int pos) {
+    switch (pos) {
+        case 0: parent->left_node=child; break;
+        case 1: parent->right_node=child; break;
+    }
 
+}
+
+//count the freq (ASCII chars)
+FreqArray *count_freq_ASCII(char text[]) {
+    FreqElem arr[128];
+    int i=0;
+
+
+
+}
 //builds the tree: input an array of nodes,
 //and array of frequencies.
-Node *build_tree(NodeArray node_arr, int freq_arr[]);
+Node *build_tree(FreqArray arr);
 
 //encoding function: opens file, collects freq, then build tree.
 //Go through the file again and write the bin code from tree into new file
 //then write out encoded, and the key.
 
-void encode(Node parent_node);
+void encode(char input_file[], char output_file[]);
+
+void decode(char input_file[], char output_file[]);
 
 
-
-
+//don't forget to check if malloc gives NULL!
 int main() {
 
     return 0;
